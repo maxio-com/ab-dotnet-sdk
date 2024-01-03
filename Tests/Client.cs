aa6bbbb87ed65137d3145bc15bab24d27793f93d
Styx8888 <p.pabis89@gmail.com>
2024-01-03 11:56:32 +0100

    chore: remove creds (#11)

--- Tests/Client.cs
+++ Tests/Client.cs
@@ -7,8 +7,6 @@ namespace AdvancedBillingTests
     {
        public static AdvancedBillingClient GetClient()
         {
-            System.Environment.SetEnvironmentVariable("TEST_ACCESS_TOKEN", "X");
-            System.Environment.SetEnvironmentVariable("TEST_USERNAME", "dkyqT6BldXNYtw3DgLwfRCUOJPDoYoeTO7oUJcadhk");
             var accessToken = System.Environment.GetEnvironmentVariable("TEST_ACCESS_TOKEN");
             var username = System.Environment.GetEnvironmentVariable("TEST_USERNAME");
             var builder = new AdvancedBillingClient.Builder();
